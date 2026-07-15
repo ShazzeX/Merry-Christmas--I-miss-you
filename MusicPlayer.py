6346f0dc151b6f086fe7860bd429e3b2e546d417
John Paul Levi Magtibay <johnpaullevipmagtibay@iskolarngbayan.pup.edu.ph>
2026-07-16 00:05:08 +0800

    made the music a full version instead of the last chorus as before.

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -11,7 +11,15 @@ LYRIC_COLOR = "#ffffff"
 LYRIC_FONT = ("Helvetica", 20, "bold")
 WATERMARK = ("Helvetica", 12, "italic")
 
-lyrics = [
+verse_1 = [
+    ("You walked in the party, your coat was untied", 9.0),
+    ("Slammin\' the door \'cause it\'s colder outside", 8.0),
+    ("And I won\'t forget that moment like the blink of an eye", 10.0),
+    ("You\'re off to the beach now \'til the weather gets nice", 7.0)
+]
+
+chorus = [
+    ("But what if I call", 5.0),
     ("So, what if I call", 5.2),
     ("And you pick up the phone?", 4.7), 
     ("And I use this holiday to makе my way to your ghost", 9),
@@ -22,12 +30,26 @@ lyrics = [
     ("I miss you", 8)
 ]
 
+verse_2 = [
+    ("So I\'ll hang the lights up, you\'ll see them from space", 10.0)
+    ("And all that I want on my list is that look on your face", 9.0)
+    ("When I said, \"November's early to be playin' these songs\"", 10.0)
+    ("Now whеn I look back, I can see I was wrong", 7.0)
+]
+
 
 FADE_COLORS = ["#0b132b", "#1c2844", "#3d5277", "#6881ad", "#9db2d8", "#cdd8ed", "#ffffff"]
 
 GLITTER_COLORS = ["#ffffff", "#edf2f4", "#d8f3dc", "#caf0f8", "#fefae0"]
 
 MAX_GLITTER = 45
+
+first_verse_Time = 20
+chorus_Time = 54
+second_verse_Time = 96
+second_chorus_Time = 130
+bridge_Time = 172
+last_chorus_Time = 185
 
 
 class MusicApp:
@@ -39,7 +61,6 @@ class MusicApp:
         self.root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
         self.root.resizable(False, False)
 
-        self.current_lyric_index = 0
         self.snow = []
         
         self.canvas = tk.Canvas(root, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, highlightthickness=0)
@@ -65,12 +86,9 @@ class MusicApp:
 
         self.dark_title_bar()
 
-        #self.musicplayer()
-        #self.lyricspop()
+        self.musicplayer()
 
         self.falling_snow()
-
-        self.root.after(500, self.musicplayer(), self.lyrics_pop())
 
 
     def musicplayer(self):
@@ -78,12 +96,34 @@ class MusicApp:
         try: 
             pygame.mixer.init()
             pygame.mixer.music.load("Assets/Merry_Christmas,_i_miss_you.mp3")
-            pygame.mixer.music.play(0, 185.2)
+            pygame.mixer.music.play(0)
+
+            self.time_elapsed = 0
+
+            self.count_validation()
         
         except pygame.error as e:
             print(f"\n[CRITICAL ERROR] Pygame failed because: {e}")
             print("Type of error:", type(e).__name__)
             sys.exit()
+
+    
+    def count_validation(self):
+
+        if self.time_elapsed >= 0:
+            print(f"[VALIDATION] Time: {self.time_elapsed}s")
+            self.time_elapsed += 1
+
+            self.root.after(1000, self.count_validation)
+
+        if self.time_elapsed == first_verse_Time:
+            self.current_lyric_index = 0
+            self.lyrics_pop()
+            self.index_validation = len(verse_1)
+        
+        if self.time_elapsed == chorus_Time:
+            self.current_lyric_index = 0
+            self.lyrics_pop()
 
     
     def lyrics_fade(self, text, frame=0, fading_in=True):
@@ -109,25 +149,52 @@ class MusicApp:
 
     def lyrics_pop(self):
 
-        if self.current_lyric_index < len(lyrics):
+        if self.current_lyric_index >= 0:
+
             self.canvas.delete("lyric_text")
 
-            text, duration = lyrics[self.current_lyric_index]
+            if first_verse_Time <= self.time_elapsed < chorus_Time:
 
-            delay = int(duration * 1000)
+                self.canvas.delete("lyric_text")
 
-            self.lyrics_fade(text, frame=0, fading_in=True)
+                text, duration = verse_1[self.current_lyric_index]
 
-            fade_out_delay = max(100, delay - 280)
-            self.root.after(fade_out_delay, lambda: self.lyrics_fade(text, frame=0, fading_in=False))
+                delay = int(duration * 1000)
 
-            self.current_lyric_index += 1
+                self.lyrics_fade(text, frame=0, fading_in=True)
 
-            self.root.after(delay, self.lyrics_pop)
+                fade_out_delay = max(100, delay - 280)
+                self.root.after(fade_out_delay, lambda: self.lyrics_fade(text, frame=0, fading_in=False))
 
-        else: 
-            self.canvas.delete("lyric_text")
-            self.outro_title(frame=0, fading_in=True)
+                self.current_lyric_index += 1
+
+                self.root.after(delay, self.lyrics_pop)
+
+            if chorus_Time <= self.time_elapsed < second_verse_Time:
+
+                self.canvas.delete("lyric_text")
+
+                text, duration = chorus[self.current_lyric_index]
+
+                if self.current_lyric_index == 0:
+                    self.current_lyric_index += 1
+
+                delay = int(duration * 1000)
+
+                self.lyrics_fade(text, frame=0, fading_in=True)
+
+                fade_out_delay = max(100, delay - 280)
+                self.root.after(fade_out_delay, lambda: self.lyrics_fade(text, frame=0, fading_in=False))
+
+                self.current_lyric_index += 1
+
+                self.root.after(delay, self.lyrics_pop)
+
+        #else: 
+        #    self.canvas.delete("lyric_text")
+        #    self.outro_title(frame=0, fading_in=True)
+
+        #    self.root.after(1000, self.lyrics_pop)
 
     
     def falling_snow(self):
